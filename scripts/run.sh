6862cad57e030a0ae195ed5febb748b864875c9c
marcmaendli@gmail.com <marcmaendli@gmail.com>
2021-05-01 14:51:31 +0200

    improve run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,11 +11,13 @@ sudo apt-get update
 
 for i in "${PACKAGES[@]}"
 do
-    dpkg -s $i || {
-        sudo apt install $i
-    } 
+    if [ $(dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed") -eq 0 ]
+    then
+        sudo apt-get install $1
+    fi
 done
 
+[ -d ansible-budgie-client ] && rm -rf ansible-budgie-client ] 
 git clone $GITREPO
 cd ansible-budgie-client
 
